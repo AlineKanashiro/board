d075e78cb3a211a5c4372d26f38695bee5331ddf
Aline Kanashiro <aline.k.kanashiro@gmail.com>
2026-02-18 20:07:18 -0300

    integrando a camada de services e camadas de dados

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -1,5 +1,6 @@
 package br.com.dio.persistence.dao;
 
+import br.com.dio.dto.BoardColumnDTO;
 import br.com.dio.persistence.entity.BoardColumnEntity;
 import br.com.dio.persistence.entity.BoardColumnKindEnum;
 import br.com.dio.persistence.entity.BoardEntity;
@@ -51,6 +52,35 @@ public class BoardColumnDAO {
                 entities.add(entity);
             }
             return entities;
+        }
+    }
+
+    public List<BoardColumnDTO> findByBoardIdWithDetails(final Long id) throws SQLException {
+        List<BoardColumnDTO> dtos = new ArrayList<>();
+        var sql =
+                """
+                SELECT bc.id,
+                       bc.name,
+                       bc.kind,
+                       COUNT(SELECT c.id FROM CARDS c WHERE c.board_column_id = bc.id) cards_amount
+                FROM BOARDS_COLUMNS bc
+                WHERE board_id = ?
+                ORDER BY `order`
+                """;
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                var dto = new BoardColumnDTO(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        findByName(resultSet.getString("kind")),
+                        resultSet.getInt("cards_amount")
+                );
+                dtos.add(dto);
+            }
+            return dtos;
         }
     }
 }
